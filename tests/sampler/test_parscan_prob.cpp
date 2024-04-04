47f6b27cf7da09dd3e73cce9aaf329cb399016a0
Vivek Kale <11766050+vlkale@users.noreply.github.com>
2024-04-04 09:23:05 -0700

    Update test_parscan_prob.cpp: fix test comments

--- tests/sampler/test_parscan_prob.cpp
+++ tests/sampler/test_parscan_prob.cpp
@@ -10,28 +10,22 @@ struct Tester {
   template <typename execution_space>
   explicit Tester(const execution_space& space) {
     //! Explicitly launch a kernel with a name, and run it 150 times with kernel
-    //! logger. Use a periodic sampling with skip rate 51. This should print
-    //! out 2 invocations, and there is a single matcher with a regular
-    //! expression to check this.
+    //! logger. Use a probabilistic sampling with 100% probability. This should print
+    //! out all 150 invocations, and there is a single matcher with a regular
+    //! expression to check the last two.
 
     int N = 1024;
     int64_t result;
-    // Kokkos::View<int64_t*> post("postfix_sum", N);
-    // Kokkos::View<int64_t*> pre("prefix_sum", N);
 
     for (int iter = 0; iter < 150; iter++) {
       result = 0;
       Kokkos::parallel_scan(
           "named kernel scan", N,
-          // Kokkos::RangePolicy<execution_space>(space, 0, N),
           *this, result);
-    }  // end timestepping loop
-  }    // end explicit Tester
+    }
+  }
 
   KOKKOS_FUNCTION void operator()(const int, const int&, bool) const {}
-  // if(isFinal) pre(i) = p_sum;
-  // p_sum += i;
-  //  if(isFinal) post(i) = p_sum;
 };
 
 static const std::vector<std::string> matchers{
