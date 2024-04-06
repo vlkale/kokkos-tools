5f60e7e7c1ad623d331b259dfa285736f508b038
Vivek Kale <11766050+vlkale@users.noreply.github.com>
2024-04-05 18:35:42 -0700

    test_parreduce.cpp: apply clang format

--- tests/sampler/test_parreduce.cpp
+++ tests/sampler/test_parreduce.cpp
@@ -6,10 +6,8 @@
 
 #include "Kokkos_Core.hpp"
 
-using ::testing::Contains;
 using ::testing::HasSubstr;
 using ::testing::Not;
-using ::testing::Times;
 
 struct Tester {
   template <typename execution_space>
@@ -71,20 +69,6 @@ TEST(SamplerTest, ktoEnvVarDefault) {
   for (const auto& matcher : matchers) {
     EXPECT_THAT(output.str(), HasSubstr(matcher));
   }
-
-  EXPECT_THAT(output.str(),
-              ::testing::Contains.Times(static_cast<int>(2),
-                                        "calling child-begin function..."));
-  EXPECT_THAT(output.str(),
-              ::testing::Contains.Times(static_cast<int>(2),
-                                        "finished with child-begin function."));
-
-  EXPECT_THAT(output.str(),
-              ::testing::Contains.Times(static_cast<int>(2),
-                                        "calling child-end function..."));
-  EXPECT_THAT(output.str(),
-              ::testing::Contains.Times(static_cast<int>(2),
-                                        "finished with child-end function."));
 
   EXPECT_THAT(output.str(), Not(HasSubstr("KokkosP: FATAL: No child library of "
                                           "sampler utility library to call")));
