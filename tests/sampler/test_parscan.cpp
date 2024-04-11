e12ace54a33c00b478078c31d6b5809d7ae7b460
Vivek Kale <11766050+vlkale@users.noreply.github.com>
2024-04-11 12:57:42 -0700

    test_parscan.cpp: using header for parscan

--- tests/sampler/test_parscan.cpp
+++ tests/sampler/test_parscan.cpp
@@ -10,25 +10,7 @@
 using ::testing::HasSubstr;
 using ::testing::Not;
 
-struct Tester {
-  template <typename execution_space>
-  explicit Tester(const execution_space& space) {
-    //! Explicitly launch a kernel with a name, and run it 15 times with kernel
-    //! logger. Use a periodic sampling with skip rate 5. This should print
-    //! out 2 invocations, and there is a single matcher with a regular
-    //! expression to check this.
-
-    long int N = 1024;
-    long int result;
-
-    for (int iter = 0; iter < 15; iter++) {
-      result = 0;
-      Kokkos::parallel_scan("named kernel scan", N, *this, result);
-    }
-  }
-
-  KOKKOS_FUNCTION void operator()(const int, long int&, bool) const {}
-};
+#include "parscan.hpp"
 
 static const std::vector<std::string> matchers{
     "KokkosP: sample 6 calling child-begin function...",
