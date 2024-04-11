603ecc46bc5b93acee4365b3eaa2d26d1ac66583
Vivek Kale <11766050+vlkale@users.noreply.github.com>
2024-04-11 10:06:38 -0700

    kp_sampler_skip.cpp: revert cerr to cout

--- common/kokkos-sampler/kp_sampler_skip.cpp
+++ common/kokkos-sampler/kp_sampler_skip.cpp
@@ -56,7 +56,7 @@ void invoke_ktools_fence(uint32_t devID) {
                 << getDeviceID(devID) << ".\n";
     }
   } else {
-    std::cerr << "KokkosP: FATAL: Kokkos Tools Programming Interface's "
+    std::cout << "KokkosP: FATAL: Kokkos Tools Programming Interface's "
                  "tool-invoked Fence is NULL!\n";
     std::abort();
     exit(-1);
